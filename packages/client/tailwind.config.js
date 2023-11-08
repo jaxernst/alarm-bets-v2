ea794a210813ca332947cf8ff9b5c9f882f8c772
jackson <jaxernst@gmail.com>
2023-11-07 23:53:13 -0800

    Config

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,7 +1,16 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jura: ["Jura", "sans-serif"],
+        digital: ["digital-clock"],
+      },
+      backgroundColor: {
+        "transparent-grey": "rgba(47, 47, 50, 0.29)", // Customize the alpha value to set the transparency level you desire
+        "highlight-transparent-grey": "rgba(60, 61, 61, 0.4)", // Customize the alpha value to set the transparency level you desire
+      },
+    },
   },
   plugins: [],
 };
