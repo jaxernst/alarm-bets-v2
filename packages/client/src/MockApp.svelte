3e216d61eea590ac32410c816c17e56b268341c2
jackson <jaxernst@gmail.com>
2023-11-08 23:12:39 -0800

    Update mock

--- packages/client/src/MockApp.svelte
+++ packages/client/src/MockApp.svelte
@@ -6,13 +6,7 @@
   import CircularProgress from "./lib/CircularProgress.svelte";
   import Lock from "./lib/icons/Lock.svelte";
 
-  let user = "0x205b";
   let wakeupObjective = true;
-
-  const activeChallenges = [
-    { name: "Daily Check In", colorClass: "text-purple-600", open: true },
-    { name: "Bet w/ Hunter B.", colorClass: "text-blue-500", open: false },
-  ];
 
   const upcomingChallenges = {
     M: [
@@ -25,23 +19,13 @@
   };
 
   const activeDays = new Set(["M", "T", "W", "Th"]);
-
-  const blueGradientClass = [
-    "border-blue-700",
-    "border-blue-600",
-    "border-blue-500",
-    "border-blue-400",
-    "border-blue-300",
-    "border-blue-200",
-    "border-blue-100",
-  ];
 </script>
 
 <div class="flex justify-center">
   <div
-    class="flex flex-col items-stretch text-zinc-700 h-screen min-w-[400px] max-w-[700px]"
+    class="flex flex-col items-stretch text-zinc-700 h-screen min-w-[400px] max-w-[500px]"
   >
-    <div class="flex-grow px-2 flex flex-col gap-3 bg-zinc-50">
+    <div class="flex-grow px-2 flex flex-col gap-6 bg-zinc-50">
       <div class="p-3 flex justify-between w-full items-center">
         <h1 class="text-cyan-500 font-bold text-xl">Alarm Bets</h1>
         <div class="stroke-cyan-500 w-6">
@@ -54,21 +38,101 @@
         {#if !wakeupObjective}
           Create new wakeup goal
         {:else}
-          <div class="text-sm py-1 text-cyan-500 flex justify-between">
+          <div class="text-sm py-2 text-cyan-500 flex justify-between">
             Wakeup Goals
-            <div>1 / 1</div>
+            <div class="text-base">+</div>
           </div>
-          <div class="p-2 flex items-center min-h-[100px]">
-            <div class="flex gap-2 w-full">
+          <div class="flex flex-col min-h-[100px] gap-2">
+            <div
+              class="flex justify-between items-stretch"
+              style="line-height:1em"
+            >
               <div
-                class="flex-grow self-center font-digital text-4xl font-bold"
-                style="line-height:1em"
+                class="px-2 py-1 font-digital text-cyan-600 text-4xl font-bold"
+              >
+                8:00 am
+              </div>
+              <div class="flex flex-col">
+                <div class="px-2 flex gap-2 text-xs font-semi py-1 justify-end">
+                  {#each ["Tu", "W", "Th", "Sa", "Su", "M"] as day, i}
+                    <div
+                      class={` ${
+                        activeDays.has(day)
+                          ? "text-cyan-400 font-bold"
+                          : "text-zinc-400"
+                      }`}
+                    >
+                      {day}
+                    </div>
+                  {/each}
+                </div>
+                <div class="flex justify-end gap-1 items-center">
+                  <div
+                    class="px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
+                  >
+                    Lvl 10
+                  </div>
+                  <div
+                    class="flex gap-1 items-center px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
+                  >
+                    42
+                    <div class="w-3 h-min fill-white">
+                      <Sun />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              class="flex justify-between items-stretch"
+              style="line-height:1em"
+            >
+              <div
+                class="px-2 pt-1 font-digital text-cyan-600 text-4xl font-bold"
               >
                 8:30 am
               </div>
-              <div class="flex-col items-end flex gap-1 text-sm text-zinc-400">
-                <div class="flex items-center gap-2">
-                  <CircularProgress progress={0.3}>12343</CircularProgress>
+              <div class="flex flex-col">
+                <div class="px-2 flex gap-2 text-xs font-semi py-1 justify-end">
+                  {#each ["Su", "M", "T", "W", "Th", "F", "Sa"] as day, i}
+                    <div
+                      class={` ${
+                        ["M", "T", "W", "Th", "F"].includes(day)
+                          ? "text-cyan-400 font-bold"
+                          : "text-zinc-400"
+                      }`}
+                    >
+                      {day}
+                    </div>
+                  {/each}
+                </div>
+                <div class="flex justify-end gap-1 items-center">
+                  <div
+                    class="px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
+                  >
+                    Lvl 5
+                  </div>
+                  <div
+                    class="flex gap-1 items-center px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
+                  >
+                    23
+                    <div class="w-3 h-min fill-white">
+                      <Sun />
+                    </div>
+                  </div>
+                  <div
+                    class="flex gap-1 items-center px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
+                  >
+                    .0123
+                    <div class="flex">
+                      <div class="w-3 h-min fill-white">
+                        <EthSymbol />
+                      </div>
+                      <div class="w-3 h-min stroke-white">
+                        <Lock />
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
@@ -76,44 +140,81 @@
         {/if}
       </div>
 
-      <div class="px-2 flex flex-col gap-2 items-stretch">
-        <div class="text-sm text-cyan-500">Upcoming Challenges</div>
-        <div class="flex flex-col gap-1">
-          {#each ["Tu", "W", "Th", "Sa", "Su", "M"] as day, i}
-            <div class="flex gap-4">
-              <div
-                class={` ${
-                  activeDays.has(day) ? "text-green-500" : "text-zinc-300"
-                }`}
-              >
-                {day}
+      <div class="p-2 flex flex-col gap-2 items-stretch">
+        <div class="flex items-center justify-between text-cyan-500">
+          <div class="text-sm">Upcoming Challenges</div>
+          <div>+</div>
+        </div>
+
+        <div class="flex flex-col gap-3">
+          <div
+            class={`flex flex-col gap-3 bg-zinc-100 shadow-lg p-2 rounded-xl w-full text-zinc-500 px-2`}
+          >
+            <div class="flex justify-between">
+              <div>
+                <div class="text-cyan-600 font-semibold">8:00am</div>
+                <div
+                  class="text-zinc-400 flex gap-1 items-center font-semibold"
+                >
+                  Wakeup Check-in
+                </div>
+                <div
+                  class="pt-2 text-zinc-400 pl-2 flex gap-1 items-center text-sm"
+                >
+                  Due in 2 hours and 29 minutes...
+                </div>
               </div>
-              <div class="flex flex-grow gap-2">
-                {#if upcomingChallenges[day]}
-                  {#each upcomingChallenges[day] as challenge}
-                    <div
-                      class={`${
-                        challenge.open
-                          ? "border border-zinc-500 text-zinc-500"
-                          : "bg-zinc-300"
-                      } flex gap-2 items-center  text-sm text-white font-semibold rounded-xl px-2 py-1`}
-                    >
-                      {challenge.time}
-                    </div>
-                  {/each}
-                {/if}
+              <div class="flex self-start gap-3">
+                <div class="flex gap-2 items-center text-sm text-green-500">
+                  +10 <div class="w-3 fill-cyan-400"><Sun /></div>
+                </div>
+                <div class="flex gap-2 items-center text-sm text-red-600">
+                  -6 <div class="w-3 fill-cyan-400"><Sun /></div>
+                </div>
               </div>
             </div>
-          {/each}
-        </div>
-      </div>
-      <div
-        class={`p-2 justify-center font-semibold text-white rounded-xl items-center flex-grow`}
-      >
-        <div class={`flex justify-between gap-4 text-zinc-500 p-3`}>
-          <div class="py-2 text-lg text-zinc-700">8:30am</div>
-          <div class="text-zinc-400 flex items-center">
-            Daily wakeup challenge
+            <div class="flex justify-end text-white">
+              <div class="px-2 bg-cyan-400 text-white rounded-lg font-semibold">
+                Check In
+              </div>
+            </div>
+          </div>
+          <div
+            class={`flex bg-zinc-100 p-2 rounded-xl justify-between w-full text-zinc-500 px-2`}
+          >
+            <div>
+              <div class="text-cyan-600 font-semibold">8:30am</div>
+              <div class="text-zinc-400 flex gap-1 items-center font-semibold">
+                Alarm Bet with jaxer.eth
+              </div>
+            </div>
+            <div class="flex flex-col justify-evenly items-end">
+              <div class="flex gap-2">
+                <div class="flex gap-1 items-center text-sm text-green-500">
+                  +10 <div class="w-3 fill-cyan-400"><Sun /></div>
+                </div>
+                <div class="flex gap-1 items-center text-sm text-red-600">
+                  -6 <div class="w-3 fill-cyan-400"><Sun /></div>
+                </div>
+              </div>
+              <div class="flex gap-2 text-sm">
+                <div class="flex gap-1 items-center text-sm text-zinc-400">
+                  <span class="text-xs">+/- </span>.002
+                  <div class="w-3 fill-cyan-400"><EthSymbol /></div>
+                </div>
+                <div class="flex gap-1 items-center text-sm text-zinc-400">
+                  .01
+                  <div class="flex">
+                    <div class="w-3 h-min fill-cyan-400">
+                      <EthSymbol />
+                    </div>
+                    <div class="w-3 h-min stroke-cyan-400">
+                      <Lock />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
