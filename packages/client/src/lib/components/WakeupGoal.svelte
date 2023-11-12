4a60e3304323c53c1dcd70c26f7c4a54bac11cce
jackson <jaxernst@gmail.com>
2023-11-11 17:10:57 -0800

    Updated dashboard

--- packages/client/src/lib/components/WakeupGoal.svelte
+++ packages/client/src/lib/components/WakeupGoal.svelte
@@ -1,100 +1,41 @@
 <script lang="ts">
-  import Sun from "../icons/Sun.svelte";
-  import EthSymbol from "../icons/EthSymbol.svelte";
-  import Lock from "../icons/Lock.svelte";
-  import { timeString } from "../util";
+	import Sun from '../icons/Sun.svelte';
+	import EthSymbol from '../icons/EthSymbol.svelte';
+	import Lock from '../icons/Lock.svelte';
+	import { timeString } from '../util';
+	import AlarmActiveDays from './ActiveDays.svelte';
+	import ActiveDays from './ActiveDays.svelte';
 
-  export let goal: {
-    time: number;
-    challengeDays: number[];
-    sunRating: number;
-    valueLocked: string;
-    sunsLocked: number;
-  };
+	export let goal: {
+		time: number;
+		challengeDays: number[];
+		suns: number;
+		valueLocked: string;
+		level: number;
+	};
 
-  let time = timeString(goal.time);
+	let time = timeString(goal.time);
 </script>
 
-<div class="flex justify-between items-stretch" style="line-height:1em">
-  <div class="px-2 py-1 font-digital text-cyan-600 text-4xl font-bold">
-    {time}
-  </div>
-  <div class="flex flex-col">
-    <div class="px-2 flex gap-2 text-xs font-semi py-1 justify-end">
-      {#each ["Su", "M", "Tu", "W", "Th", "Sa", "Su"] as day, i}
-        <div
-          class={` ${
-            goal.challengeDays.includes(i + 1)
-              ? "text-cyan-400 font-bold"
-              : "text-zinc-400"
-          }`}
-        >
-          {day}
-        </div>
-      {/each}
-    </div>
-    <div class="flex justify-end gap-1 items-center">
-      <div
-        class="px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
-      >
-        Lvl 10
-      </div>
-      <div
-        class="flex gap-1 items-center px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
-      >
-        42
-        <div class="w-3 h-min fill-white">
-          <Sun />
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<div class="flex justify-between items-stretch" style="line-height:1em">
-  <div class="px-2 pt-1 font-digital text-cyan-600 text-4xl font-bold">
-    8:30 am
-  </div>
-  <div class="flex flex-col">
-    <div class="px-2 flex gap-2 text-xs font-semi py-1 justify-end">
-      {#each ["Su", "M", "T", "W", "Th", "F", "Sa"] as day, i}
-        <div
-          class={` ${
-            ["M", "T", "W", "Th", "F"].includes(day)
-              ? "text-cyan-400 font-bold"
-              : "text-zinc-400"
-          }`}
-        >
-          {day}
-        </div>
-      {/each}
-    </div>
-    <div class="flex justify-end gap-1 items-center">
-      <div
-        class="px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
-      >
-        Lvl 5
-      </div>
-      <div
-        class="flex gap-1 items-center px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
-      >
-        23
-        <div class="w-3 h-min fill-white">
-          <Sun />
-        </div>
-      </div>
-      <div
-        class="flex gap-1 items-center px-2 text-xs rounded-full bg-cyan-400 text-white font-semibold"
-      >
-        .0123
-        <div class="flex">
-          <div class="w-3 h-min fill-white">
-            <EthSymbol />
-          </div>
-          <div class="w-3 h-min stroke-white">
-            <Lock />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="flex flex-col relative bg-gradient-to-r from-cyan-400 to-cyan-500 rounded-xl p-1">
+	<div class="flex justify-between gap-1 items-center p-1 text-white">
+		<div class="text-xs rounded-full font-semibold">Lvl 10</div>
+		<div class="flex gap-1 items-center text-xs rounded-full font-semibold">
+			42
+			<div class="w-3 h-min fill-white">
+				<Sun />
+			</div>
+		</div>
+	</div>
+	<div class="py-2">
+		<div
+			class="px-2 py-1 font-digital text-cyan-100 text-4xl font-bold text-center pt-1"
+			style="line-height:1em"
+		>
+			{time}
+		</div>
+	</div>
+	<div class="flex flex-col px-2">
+		<ActiveDays activeDays={goal.challengeDays} />
+	</div>
 </div>
--- packages/client/src/routes/+page.svelte
+++ packages/client/src/routes/+page.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
 	import { mud } from '$lib/mud/mudStore';
-	import MockApp from '$lib/MockApp.svelte';
-	$: console.log($mud);
+	import MockApp from '$lib/Dashboard.svelte';
+	import NavBar from '$lib/components/NavBar.svelte';
+	import Wallet from '$lib/icons/Wallet.svelte';
+	import Dashboard from '$lib/Dashboard.svelte';
 </script>
 
-<MockApp />
+<div class="flex justify-center ">
+	<div class="flex flex-col text-zinc-700 h-screen overflow-hidden min-w-[400px] max-w-[500px]">
+		<div class="p-3 flex justify-between w-full items-center">
+			<h1 class="text-cyan-500 font-bold text-xl">Alarm Bets</h1>
+			<div class="stroke-cyan-500 w-6">
+				<Wallet />
+			</div>
+		</div>
+		<div class="flex-grow overflow-hidden">
+			<Dashboard />
+		</div>
+		<NavBar />
+	</div>
+</div>
