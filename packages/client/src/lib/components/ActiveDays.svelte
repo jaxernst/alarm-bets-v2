52c0624b36002a18c9160a599ea77450aa44a80e
jackson <jaxernst@gmail.com>
2023-11-11 17:10:27 -0800

    Active days component

---
+++ packages/client/src/lib/components/ActiveDays.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let activeDays: number[];
+</script>
+
+<div class="px-2 flex gap-2 text-xs font-semi py-1 justify-end">
+	{#each ['Su', 'M', 'Tu', 'W', 'Th', 'Sa', 'Su'] as day, i}
+		<div class={` ${activeDays.includes(i + 1) ? 'text-white font-bold' : 'text-cyan-300'}`}>
+			{day}
+		</div>
+	{/each}
+</div>
