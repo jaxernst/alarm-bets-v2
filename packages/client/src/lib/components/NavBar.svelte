e341226c9b178ac5e4c792b7c2bddcb704e012cf
jackson <jaxernst@gmail.com>
2023-11-13 21:57:46 -0800

    Add route tag

--- packages/client/src/lib/components/NavBar.svelte
+++ packages/client/src/lib/components/NavBar.svelte
@@ -1,5 +1,7 @@
-<ul class="flex w-full justify-evenly py-2 bg-cyan-500 text-white font-bold rounded-t-lg">
-	<li>Dashboard</li>
+<ul
+	class="flex w-full justify-evenly py-2 bg-cyan-500 text-white font-bold rounded-t-lg min-w-[400px]"
+>
+	<li><a href="/">Dashboard</a></li>
 	<li>Challenges</li>
 	<li>Friends</li>
 </ul>
