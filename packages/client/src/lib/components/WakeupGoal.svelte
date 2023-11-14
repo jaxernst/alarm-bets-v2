8f998eed1e81db9b46b07a5b72e123fc35392298
jackson <jaxernst@gmail.com>
2023-11-13 21:57:38 -0800

    User real wakeup goals

--- packages/client/src/lib/components/WakeupGoal.svelte
+++ packages/client/src/lib/components/WakeupGoal.svelte
@@ -1,27 +1,28 @@
 <script lang="ts">
-	import Sun from '../icons/Sun.svelte';
-	import EthSymbol from '../icons/EthSymbol.svelte';
-	import Lock from '../icons/Lock.svelte';
-	import { timeString } from '../util';
-	import AlarmActiveDays from './ActiveDays.svelte';
-	import ActiveDays from './ActiveDays.svelte';
+	import Sun from '../icons/Sun.svelte'
+	import EthSymbol from '../icons/EthSymbol.svelte'
+	import Lock from '../icons/Lock.svelte'
+	import { timeString } from '../util'
+	import AlarmActiveDays from './ActiveDays.svelte'
+	import ActiveDays from './ActiveDays.svelte'
+	import type { Entity } from '@latticexyz/recs'
 
 	export let goal: {
-		time: number;
-		challengeDays: number[];
-		suns: number;
-		valueLocked: string;
-		level: number;
-	};
+		entity: Entity
+		time: number
+		suns: number
+		level: number
+	}
 
-	let time = timeString(goal.time);
+	let time = timeString(goal.time)
+	let challengeDays = [2, 3, 5]
 </script>
 
 <div class="flex flex-col relative bg-gradient-to-r from-cyan-400 to-cyan-500 rounded-xl p-1">
 	<div class="flex justify-between gap-1 items-center p-1 text-white">
-		<div class="text-xs rounded-full font-semibold">Lvl 10</div>
+		<div class="text-xs rounded-full font-semibold">Lvl {goal.level}</div>
 		<div class="flex gap-1 items-center text-xs rounded-full font-semibold">
-			42
+			{goal.suns}
 			<div class="w-3 h-min fill-white">
 				<Sun />
 			</div>
@@ -29,13 +30,13 @@
 	</div>
 	<div class="py-2">
 		<div
-			class="px-2 py-1 font-digital text-cyan-100 text-4xl font-bold text-center pt-1"
+			class=" px-6 py-1 font-digital text-cyan-100 text-4xl font-bold text-center pt-1"
 			style="line-height:1em"
 		>
 			{time}
 		</div>
 	</div>
 	<div class="flex flex-col px-2">
-		<ActiveDays activeDays={goal.challengeDays} />
+		<ActiveDays activeDays={challengeDays} />
 	</div>
 </div>
