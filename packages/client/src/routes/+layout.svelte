0da1aa7b74ef2ca448baa737b1b5c82d51c599e1
jackson <jaxernst@gmail.com>
2023-11-13 21:57:28 -0800

    Update layout

--- packages/client/src/routes/+layout.svelte
+++ packages/client/src/routes/+layout.svelte
@@ -1,5 +1,25 @@
 <script>
-  import "../app.css"
+	import NavBar from '$lib/components/NavBar.svelte';
+	import Wallet from '$lib/icons/Wallet.svelte';
+	import { mud } from '$lib/mud/mudStore';
+	import '../app.css';
 </script>
 
-<slot />
+{#if !$mud}
+	Loading MUD App...
+{:else}
+	<div class="flex justify-center">
+		<div class={`flex flex-col text-zinc-700 h-screen overflow-hidden max-w-[500px]`}>
+			<div class="p-3 flex justify-between w-full items-center">
+				<h1 class="text-cyan-500 font-bold text-xl">Alarm Bets</h1>
+				<div class="stroke-cyan-500 w-6">
+					<Wallet />
+				</div>
+			</div>
+			<div class="flex-grow overflow-hidden">
+				<slot />
+			</div>
+			<NavBar />
+		</div>
+	</div>
+{/if}
