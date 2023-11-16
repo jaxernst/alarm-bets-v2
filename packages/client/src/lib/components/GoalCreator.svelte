1b7926fb8eacfc4bf0ca06861bb7ff13f93f1c66
jackson <jaxernst@gmail.com>
2023-11-16 00:08:20 -0800

    Smoothen transitions

--- packages/client/src/lib/components/GoalCreator.svelte
+++ packages/client/src/lib/components/GoalCreator.svelte
@@ -3,11 +3,12 @@
 	import Plus from './Plus.svelte'
 	import GradientCard from './design-sys/GradientCard.svelte'
 	import { fade, scale, slide } from 'svelte/transition'
-	import { cubicInOut } from 'svelte/easing'
+	import { cubicInOut, cubicOut } from 'svelte/easing'
 	import { localTzOffsetHrs, parseTimeString, readableTimezone } from '$lib/util'
 	import { mud } from '$lib/mud/mudStore'
 	import Sun from '$lib/icons/Sun.svelte'
 	import { onMount } from 'svelte'
+	import { tweened } from 'svelte/motion'
 
 	export let onGoalCreated = () => {}
 	export let onClose = () => {}
@@ -70,8 +71,8 @@
 		const alarmTime = parseTimeString($alarmTimeInput)
 		submitLoading = true
 		try {
-			await new Promise((resolve) => setTimeout(resolve, 500))
-			await $mud.systemCalls.createWakeupObjective(alarmTime, playerTimezoneOffset)
+			await new Promise((resolve) => setTimeout(resolve, 2000))
+			// await $mud.systemCalls.createWakeupObjective(alarmTime, playerTimezoneOffset)
 			onGoalCreated()
 		} catch (e) {
 			console.error(e)
@@ -79,6 +80,21 @@
 			inputActive = false
 			submitLoading = false
 		}
+	}
+
+	let slideOutBoxWidth = tweened(0, {
+		duration: 500,
+		easing: cubicOut
+	})
+
+	function toggleWidth() {
+		$slideOutBoxWidth = $slideOutBoxWidth === 0 ? 100 : 0
+	}
+
+	$: if (alarmTimeInputValid) {
+		$slideOutBoxWidth = 100
+	} else {
+		$slideOutBoxWidth = 0
 	}
 </script>
 
@@ -90,7 +106,7 @@
 		}`}
 	>
 		{#if submitLoading}
-			<div class=" w-5 animate-spin" transition:fade={{ delay: 370 }}>
+			<div class="fill-white w-5 animate-spin" transition:scale={{ delay: 100 }}>
 				<Sun />
 			</div>
 		{:else}
@@ -107,50 +123,54 @@
 						</div>
 					</div>
 				{:else}
-					<div transition:slide class="flex row-start-1 col-start-1 gap-3 items-center">
-						<input
-							type="text"
-							placeholder="At what time would you like to wake up?"
-							class="grid-cols-1 font-semibold text-center w-full bg-transparent placeholder-cyan-200 outline-none"
-							bind:value={$alarmTimeInput}
-						/>
-						{#if alarmTimeInputValid}
-							<div
-								class="flex justify-evenly items-center px-2 bg-cyan-400 text-cyan-50 rounded-full"
-								in:slide={{ axis: 'x', easing: cubicInOut }}
-								on:click|preventDefault|stopPropagation={(e) => {
-									timezoneConfirmed = true
-								}}
-							>
-								{#if !timezoneConfirmed}
-									<button class=" whitespace-nowrap text-center py-1 px-2 text-sm">
-										Confirm your primary timezone
-									</button>
-								{:else}
-									<div
-										transition:slide={{ axis: 'x' }}
-										class="min-w-[150px] flex grid-col-1 justify-between items-center gap-1"
-									>
-										<button on:click={() => playerTimezoneOffset--} class="px-2 font-semibold"
-											>{'<'}</button
-										>
-										<p class="text-center min-w-[240px] py-1 px-2 text-sm">
-											{readableTimezone(playerTimezoneOffset)}
-										</p>
-										<button on:click={() => playerTimezoneOffset++} class="px-2 font-semibold"
-											>{'>'}</button
-										>
+					<div class="flex items-center">
+						<div
+							transition:slide
+							class="flex flex-col w-full row-start-1 col-start-1 gap-3 overflow-hidden items-center"
+						>
+							<input
+								type="text"
+								placeholder="At what time would you like to wake up?"
+								class={`self-stretch flex-grow font-semibold text-center bg-transparent placeholder-cyan-200 outline-none`}
+								bind:value={$alarmTimeInput}
+							/>
+							{#if alarmTimeInputValid}
+								<div
+									transition:slide
+									class={` ${
+										readyToSubmit ? 'border border-green-300' : ''
+									} transition-all flex-grow items-center justify-evenly min-w-[66%] oveflow-hidden px-2 bg-cyan-400 text-cyan-50 rounded-full`}
+									on:click|preventDefault|stopPropagation={(e) => {
+										timezoneConfirmed = true
+									}}
+								>
+									<div class={` flex gap-2 justify-between py-1 px-2 items-center transition-all `}>
+										{#if !timezoneConfirmed}
+											<button class="text-center w-full" on:click={() => (timezoneConfirmed = true)}
+												>Confirm your primary timezone</button
+											>
+										{:else}
+											<button on:click={() => playerTimezoneOffset--} class="font-semibold"
+												>{'<'}</button
+											>
+											<p class="overflow-auto text-sm text-center flex-grow">
+												{readableTimezone(playerTimezoneOffset)}
+											</p>
+											<button on:click={() => playerTimezoneOffset++} class="font-semibold"
+												>{'>'}</button
+											>
+										{/if}
 									</div>
-								{/if}
-							</div>
-						{/if}
+								</div>
+							{/if}
+						</div>
 						{#if readyToSubmit}
 							<button
+								transition:slide={{ axis: 'x', easing: cubicInOut }}
 								on:click={() => submitGoal()}
-								class={`w-10 first-letter:animate-pulse`}
-								transition:slide
+								class={`p-2 bg-cyan-600 hover:bg-cyan-300 transition-all duration-150 rounded-full`}
 							>
-								<div class="w-3 fill-green-300">
+								<div class="w-5 animate-pulse fill-green-200">
 									<Plus />
 								</div>
 							</button>
