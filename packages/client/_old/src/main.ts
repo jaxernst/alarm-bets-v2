e58800082af604340607139c99107a571482aa10
jackson <jaxernst@gmail.com>
2023-11-15 22:39:42 -0800

    Delete old

--- packages/client/_old/src/main.ts
+++
@@ -1,8 +0,0 @@
-import "./app.css";
-import PreApp from "./PreApp.svelte";
-
-const app = new PreApp({
-  target: document.getElementById("app"),
-});
-
-export default app;
--- packages/client/_old/svelte.config.ts
+++
@@ -1,7 +0,0 @@
-import preprocess from "svelte-preprocess";
-
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess({ typescript: true }),
-};
