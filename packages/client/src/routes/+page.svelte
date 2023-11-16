c6c8a6a3ba3e5f841efd4d36cb02b1776b70c261
jackson <jaxernst@gmail.com>
2023-11-15 22:40:08 -0800

    Styling

--- packages/client/src/routes/+page.svelte
+++ packages/client/src/routes/+page.svelte
@@ -49,7 +49,7 @@
 	let showGoalCreator = false
 </script>
 
-<div class="h-full flex flex-col gap-6">
+<div class="h-full flex flex-col gap-10">
 	<div class="w-full px-2">
 		{#if showGoalCreator}
 			<div transition:slide>
@@ -80,20 +80,22 @@
 		</div>
 	</div>
 
-	<div class="px-2 flex items-center gap-3 text-cyan-500">
-		<div class="rounded-full px-2 py-1 bg-cyan-600 text-cyan-50">Active Challenges</div>
-		<div class="rounded-full px-2 py-1">Available Challenges</div>
-		<div class="rounded-full px-2 py-1">Leaderboard</div>
-	</div>
+	<div class="flex gap-4 flex-col flex-grow bg-slate-100 rounded-t-2xl py-3">
+		<div class="px-2 flex items-center gap-3 text-cyan-400">
+			<div class="rounded-full px-2 py-1 bg-cyan-400 text-cyan-50">Active Challenges</div>
+			<div class="rounded-full px-2 py-1">Available Challenges</div>
+			<div class="rounded-full px-2 py-1">Leaderboard</div>
+		</div>
 
-	<div class="px-2 flex flex-col items-stretch overflow-hidden">
-		{#if !wakeupChallenges.length}
-			<div class="p-3 bg-slate-50 rounded-xl text-zinc-400 text-sm">No active challenges ...</div>
-		{:else}
-			<div class="overflow-y-auto">
-				<ChallengeTimeline challenges={wakeupChallenges} />
-			</div>
-		{/if}
+		<div class="px-2 flex flex-col items-stretch overflow-hidden">
+			{#if !wakeupChallenges.length}
+				<div class="p-3 text-zinc-400 text-sm">No active challenges ...</div>
+			{:else}
+				<div class="overflow-y-auto">
+					<ChallengeTimeline challenges={wakeupChallenges} />
+				</div>
+			{/if}
+		</div>
 	</div>
 </div>
 
