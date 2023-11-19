b9130b71cad600b07fe73332bfa0bd3a394f5c28
jackson <jaxernst@gmail.com>
2023-11-18 16:41:18 -0800

    Refactor

--- packages/client/src/lib/components/ActiveDays.svelte
+++ packages/client/src/lib/components/ActiveDays.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-	export let activeDays: number[];
+	export let activeDays: number[]
+	export let activeDayClass = 'text-white font-bold'
+	export let inactiveDayClass = 'text-cyan-300'
 </script>
 
 <div class="px-2 flex gap-2 text-xs font-semi py-1 justify-end">
 	{#each ['Su', 'M', 'Tu', 'W', 'Th', 'Sa', 'Su'] as day, i}
-		<div class={` ${activeDays.includes(i + 1) ? 'text-white font-bold' : 'text-cyan-300'}`}>
+		<div class={` ${activeDays.includes(i + 1) ? activeDayClass : inactiveDayClass}`}>
 			{day}
 		</div>
 	{/each}
