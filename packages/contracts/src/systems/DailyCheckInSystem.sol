3e0ac6ea929433f06db31c4fcd679cb300e68834
jackson <jaxernst@gmail.com>
2023-11-30 23:17:29 -0800

    Use new AlarmSchedule system

--- packages/contracts/src/systems/DailyCheckInSystem.sol
+++ packages/contracts/src/systems/DailyCheckInSystem.sol
@@ -2,50 +2,34 @@
 pragma solidity >=0.8.21;
 
 import { getUniqueEntity } from "@latticexyz/world-modules/src/modules/uniqueentity/getUniqueEntity.sol";
+import { SystemSwitch } from "@latticexyz/world-modules/src/utils/SystemSwitch.sol";
 import { System } from "@latticexyz/world/src/System.sol";
-import { AlarmSchedule } from "../library/AlarmSchedule.sol";
 import { Status } from "../codegen/common.sol";
-import { 
-  WakeupObjective,
-  Creator,
-  Timezone,
-  AlarmTime,
-  Suns,
-  ChallengeStatus,
-  WakeupChallenge, 
-  ChallengeName, 
-  ExpirationTime, 
-  TargetWakeupObjective,
-  ChallengeDays,
-  StartTime,
-  SunsStaked
-} from "../codegen/index.sol";
+import { AlarmScheduleSystem } from "./AlarmScheduleSystem.sol";
+import { WakeupObjective, Creator, Timezone, AlarmTime, Suns, ChallengeStatus, WakeupChallenge, ChallengeName, ExpirationTime, TargetWakeupObjective, ChallengeDays, StartTime, SunsStaked } from "../codegen/index.sol";
+import { IWorld } from "../codegen/world/IWorld.sol";
 
 contract DailyCheckInSystem is System {
-  using AlarmSchedule for AlarmSchedule.Schedule;
-
   string public constant CHALLENGE_NAME = "Daily Check In";
-  uint public constant SUBMISSION_WINDOW = 15 minutes;
+  uint32 public constant SUBMISSION_WINDOW = 15 minutes;
   uint public constant SUN_REWARD_PER_DAY = 10;
   uint public constant SUN_COST_PER_DAY = 6;
 
-  mapping(bytes32 => AlarmSchedule.Schedule) schedules;
-
   function DailyCheckIn_enter(bytes32 wakeupObjective, uint32 numWeeks, uint8[] memory challengeDays) public {
     int8 timezoneHrs = Timezone.get(wakeupObjective);
-    uint alarmTime = AlarmTime.get(wakeupObjective);
+    uint32 alarmTime = AlarmTime.get(wakeupObjective);
     address creator = Creator.get(wakeupObjective);
     uint balanceSuns = Suns.get(wakeupObjective);
-    
+
     uint costSuns = numWeeks * challengeDays.length * SUN_COST_PER_DAY;
 
     require(WakeupObjective.get(wakeupObjective), "Not a wakeup objective");
     require(creator == _msgSender(), "creator only");
     require(balanceSuns >= costSuns, "Insufficient Sun balance");
-    
+
     // Apply the entry fee for the challenge
     Suns.set(wakeupObjective, balanceSuns - costSuns);
-    
+
     // Create challenge entity
     bytes32 challengeId = getUniqueEntity();
     ChallengeName.set(challengeId, CHALLENGE_NAME);
@@ -60,7 +44,12 @@ contract DailyCheckInSystem is System {
     uint expiration = block.timestamp + numWeeks * 1 weeks;
     ExpirationTime.set(challengeId, expiration);
 
-    _startSchedule(challengeId, alarmTime, challengeDays, int(timezoneHrs) * 1 hours);
+    SystemSwitch.call(
+      abi.encodeCall(
+        IWorld(_world()).newAlarmSchedule,
+        (challengeId, alarmTime, SUBMISSION_WINDOW, timezoneHrs, uint32(expiration), challengeDays)
+      )
+    );
   }
 
   function DailyCheckIn_confirmWakeup(bytes32 challengeId) public {
@@ -69,21 +58,7 @@ contract DailyCheckInSystem is System {
 
     require(challengeCreator == _msgSender(), "Only creator can confirm wakeup");
     require(expiration > block.timestamp, "Challenge expired");
-    require(schedules[challengeId].inSubmissionWindow(), "Not in submission window");
 
-    schedules[challengeId].recordEntry();
-  }
-
-  function _startSchedule(
-    bytes32 challengeId,
-    uint alarmTime,
-    uint8[] memory challengeDays,
-    int timezoneSeconds
-  ) private {
-    {
-      // Create new schedule
-      schedules[challengeId].init(alarmTime, challengeDays, SUBMISSION_WINDOW, timezoneSeconds);
-      schedules[challengeId].start();
-    }
+    SystemSwitch.call(abi.encodeCall(IWorld(_world()).recordEntry, (challengeId)));
   }
 }
