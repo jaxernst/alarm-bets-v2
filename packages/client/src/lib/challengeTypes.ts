f4f3732234692d6c3e32d5a6c807e4fec8401441
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-12-04 18:43:48 -0800

    Add challenge types

---
+++ packages/client/src/lib/challengeTypes.ts
@@ -0,0 +1,48 @@
+type Currency = 'suns' | 'eth'
+
+type FixedStake<T extends Currency> = {
+	type: 'fixed'
+	amount: number
+	currency: T
+}
+
+type VariablStake<T extends Currency> = {
+	type: 'variable'
+	minAmount: number
+	currency: T
+}
+
+type Stake<T extends Currency> = FixedStake<T> | VariablStake<T>
+
+export type ChallengeInfo = {
+	name: string
+	description: string
+	requiredLevel: number
+	sunEntryStake: Stake<'suns'>
+	ethEntryStake?: Stake<'eth'>
+	sunReward: {
+		amount: number
+		currency: 'suns'
+	}
+
+	attestionRequired?: boolean
+}
+
+export const challengeTypes: ChallengeInfo[] = [
+	{
+		name: 'Daily Check in',
+		requiredLevel: 1,
+		sunReward: { amount: 10, currency: 'suns' },
+		sunEntryStake: { type: 'fixed', currency: 'suns', amount: 5 },
+		description:
+			'Wakeup before your wakeup goal time and check in to earn suns. If you miss your wakeup on the days you select, no reward is given'
+	},
+	{
+		name: 'Wakeup Wordle',
+		attestionRequired: true,
+		sunReward: { amount: 35, currency: 'suns' },
+		sunEntryStake: { type: 'fixed', currency: 'suns', amount: 18 },
+		requiredLevel: 5,
+		description: 'Wakeup and solve a puzzle before your goal time to earn suns'
+	}
+]
